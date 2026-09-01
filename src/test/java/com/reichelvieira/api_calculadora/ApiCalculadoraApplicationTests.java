7cf820bf5c7960835321b25f0f35b0104e938fcf
Eduardo Reichel Vieira <eduardoreichel05@gmail.com>
2026-08-31 21:28:44 -0300

    Initial commit

---
+++ src/test/java/com/reichelvieira/api_calculadora/ApiCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reichelvieira.api_calculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
