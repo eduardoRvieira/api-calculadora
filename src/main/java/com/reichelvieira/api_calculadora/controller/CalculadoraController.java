b573e048bd54b9ffcb489b3300947708f4914c3f
Eduardo Reichel Vieira <146209138+eduardoRvieira@users.noreply.github.com>
2026-09-01 10:05:09 -0300

    toques finais

--- src/main/java/com/reichelvieira/api_calculadora/controller/CalculadoraController.java
+++ src/main/java/com/reichelvieira/api_calculadora/controller/CalculadoraController.java
@@ -1,5 +1,35 @@
 package com.reichelvieira.api_calculadora.controller;
 
+import com.reichelvieira.api_calculadora.dto.CalculadoraRequisicao;
+import com.reichelvieira.api_calculadora.dto.CalculadoraResposta;
+import com.reichelvieira.api_calculadora.service.CalculadoraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/calculadora")
 public class CalculadoraController {
 
+    @Autowired
+    CalculadoraService servico;
+
+    @PostMapping("calculadora/somar")
+    public CalculadoraResposta somar(@RequestBody CalculadoraRequisicao requisicao){
+        return servico.adicao(requisicao);
+    }
+
+    @PostMapping("calculadora/subtrair")
+    public CalculadoraResposta subtrair(@RequestBody CalculadoraRequisicao requisicao){
+        return servico.subtracao(requisicao);
+    }
+
+    @PostMapping("calculacora/multiplicar")
+    public CalculadoraResposta multiplicar(@RequestBody CalculadoraRequisicao requisicao){
+        return servico.multiplicacao(requisicao);
+    }
+
+    @PostMapping("calculadora/dividir")
+    public CalculadoraResposta dividir(@RequestBody CalculadoraRequisicao requisicao){
+        return servico.divisao(requisicao);
+    }
 }
--- src/main/java/com/reichelvieira/api_calculadora/service/CalculadoraService.java
+++ src/main/java/com/reichelvieira/api_calculadora/service/CalculadoraService.java
@@ -8,24 +8,24 @@ import org.springframework.stereotype.Service;
 public class CalculadoraService {
    public CalculadoraResposta adicao(CalculadoraRequisicao requisicao){
         double resultado = requisicao.getN1() + requisicao.getN2();
-        return new CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "Adição", resultado);
+        return new CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "soma", resultado);
    }
 
    public CalculadoraResposta subtracao(CalculadoraRequisicao requisicao){
        double resultado = requisicao.getN1() - requisicao.getN2();
-       return new CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "Subtração", resultado);
+       return new CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "subtração", resultado);
    }
 
    public CalculadoraResposta multiplicacao(CalculadoraRequisicao requisicao){
        double resultado = requisicao.getN1() * requisicao.getN2();
-       return new CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "Multiplicação", resultado);
+       return new CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "multiplicação", resultado);
    }
 
    public CalculadoraResposta divisao(CalculadoraRequisicao requisicao){
-       double resultado = requisicao.getN1()/requisicao.getN2();
-       if (requisicao.getN2() == 0){
-           resultado = 0;
+       double resultado = 0;
+       if (requisicao.getN2() != 0){
+           resultado = requisicao.getN1()/requisicao.getN2();
        }
-       return new CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "Divisão", resultado);
+       return new CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "divisão", resultado);
    }
 }
