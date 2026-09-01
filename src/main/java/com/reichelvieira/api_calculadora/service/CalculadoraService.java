1b93ff0a14317357284c752aa247e110e9070d16
Eduardo Reichel Vieira <146209138+eduardoRvieira@users.noreply.github.com>
2026-09-01 09:26:14 -0300

    classe controller

--- src/main/java/com/reichelvieira/api_calculadora/service/CalculadoraService.java
+++ src/main/java/com/reichelvieira/api_calculadora/service/CalculadoraService.java
@@ -4,8 +4,26 @@ import com.reichelvieira.api_calculadora.dto.CalculadoraRequisicao;
 import com.reichelvieira.api_calculadora.dto.CalculadoraResposta;
 
 public class CalculadoraService {
-   public CalculadoraResposta soma(CalculadoraRequisicao requisicao){
-        double soma = requisicao.getN1() + requisicao.getN2();
-        return new CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "Soma", soma);
+   public CalculadoraResposta adicao(CalculadoraRequisicao requisicao){
+        double resultado = requisicao.getN1() + requisicao.getN2();
+        return new CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "Adição", resultado);
+   }
+
+   public CalculadoraResposta subtracao(CalculadoraRequisicao requisicao){
+       double resultado = requisicao.getN1() - requisicao.getN2();
+       return new CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "Subtração", resultado);
+   }
+
+   public CalculadoraResposta multiplicacao(CalculadoraRequisicao requisicao){
+       double resultado = requisicao.getN1() * requisicao.getN2();
+       return new CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "Multiplicação", resultado);
+   }
+
+   public CalculadoraResposta divisao(CalculadoraRequisicao requisicao){
+       double resultado = requisicao.getN1()/requisicao.getN2();
+       if (requisicao.getN2() == 0){
+           resultado = 0;
+       }
+       return new CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "Divisão", resultado);
    }
 }
