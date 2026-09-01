8f8473c750a524160dc9438fd59924c9d6f63dd3
Eduardo Reichel Vieira <eduardoreichel05@gmail.com>
2026-08-31 21:48:26 -0300

    teste de soma

--- src/main/java/com/reichelvieira/api_calculadora/service/CalculadoraService.java
+++ src/main/java/com/reichelvieira/api_calculadora/service/CalculadoraService.java
@@ -1,4 +1,11 @@
 package com.reichelvieira.api_calculadora.service;
 
+import com.reichelvieira.api_calculadora.dto.CalculadoraRequisicao;
+import com.reichelvieira.api_calculadora.dto.CalculadoraResposta;
+
 public class CalculadoraService {
+   public CalculadoraResposta soma(CalculadoraRequisicao requisicao){
+        double soma = requisicao.getN1() + requisicao.getN2();
+        return CalculadoraResposta(requisicao.getN1(), requisicao.getN2(), "Soma", soma);
+   }
 }
